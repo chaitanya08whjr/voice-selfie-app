614434f812b59590ffc6c506e46a1950b704b9a9
chaitanya08whjr <74759279+chaitanya08whjr@users.noreply.github.com>
2021-02-02 13:02:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var recognition = new SpeechRecognition();
+
+function start() {
+    document.getElementById("textbox").innerHTML = "";
+    recognition.start();
+}
+recognition.onresult = function (event) {
+    console.log(event);
+    var Content = event.results[0][0].transcript;
+    document.getElementById("textbox").innerHTML = Content;
+    console.log(Content);
+    if (Content == "take my selfie") {
+        speak();
+    }
+
+}
+
+function speak() {
+    var synth = window.speechSynthesis;
+    var speak_data = "taking your selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+    Webcam.attach(camera);
+}
